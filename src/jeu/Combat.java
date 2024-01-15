efd0313c805b65f2c1504f87baac227005ea699d
Francois <jeremy.francois@accenture.com>
2024-01-15 11:40:26 +0100

    combat avec armure fonctionnel

--- src/jeu/Combat.java
+++ src/jeu/Combat.java
@@ -10,24 +10,24 @@ public class Combat {
     public static void combattre(Personnage joueur, Personnage adversaire){
 
         String vainqueur = "";
-//        int attaqueAleatoireJoueur = (int)(Math.random()* joueur.attaque+1);
-//        int attaqueAleatoireAdversaire = (int)(Math.random()* adversaire.attaque+1);
+
 
         while (joueur.pv > 0 && adversaire.pv > 0){
             if(joueur.pv > 0){
                 int attaqueAleatoireJoueur = ((int) (Math.random()* joueur.attaque )+1)- adversaire.armure;
-                boolean attaqueNegativeJoueur = true;
 
-                if(attaqueAleatoireJoueur <= 0 ) {attaqueAleatoireJoueur += 1;}
+                if(attaqueAleatoireJoueur <= 0 )  attaqueAleatoireJoueur = 1;
                 adversaire.pv -= attaqueAleatoireJoueur ;
-                System.out.println(attaqueAleatoireJoueur);
+
+                if(adversaire.pv <= 0) adversaire.pv = 0;
                 System.out.println("BIM ! PV de " + adversaire.nom + " = "+ adversaire.pv);
             }
             if(adversaire.pv > 0){
                 int attaqueAleatoireAttaquant = ((int)(Math.random()* adversaire.attaque)+1)- joueur.armure;
-                if(attaqueAleatoireAttaquant <= 0) {attaqueAleatoireAttaquant += 1;}
+                if(attaqueAleatoireAttaquant <= 0) {attaqueAleatoireAttaquant = 1;}
                 joueur.pv -= attaqueAleatoireAttaquant;
-                System.out.println(attaqueAleatoireAttaquant);
+
+                if(joueur.pv <= 0) joueur.pv = 0;
                 System.out.println("BAM !  PV de " + joueur.nom + " = "+ joueur.pv );
             }
 
